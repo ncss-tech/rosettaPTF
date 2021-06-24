05f95d162343294fa007b8696eb65fad8f6355f2
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-06-24 09:58:18 -0700

    Add pip argument

--- R/install_rosetta.R
+++ R/install_rosetta.R
@@ -4,9 +4,9 @@
 #' @param envname The name, or full path, of the environment in which Python packages are to be installed. When `NULL` (the default), the active environment as set by the `RETICULATE_PYTHON_ENV` variable will be used; if that is unset, then the `"r-reticulate"` environment will be used.
 #' @param method `"auto"`, `"virtualenv"`, or `"conda"`; Default: `"auto"`
 #' @param conda Default: `"auto"`
-#'
+#' @param pip _logical_. Use pip for package installation? This is only relevant when Conda environments are used, as otherwise packages will be installed from the Conda repositories.
 #' @export
 #' @importFrom reticulate py_install
-install_rosetta <- function(envname = NULL, method = "auto", conda = "auto") {
-  reticulate::py_install("rosetta-soil", envname = envname, method = method, conda = conda)
+install_rosetta <- function(envname = NULL, method = "auto", conda = "auto", pip = FALSE) {
+  reticulate::py_install("rosetta-soil", envname = envname, method = method, conda = conda, pip = pip)
 }
