11bb4bfc31605cc87673cd5261626de36ba686ee
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-06-24 13:49:48 -0700

    Install/run Rosetta now works on Windows/CCE machines

--- misc/test_cce_install-rosetta.R
+++ misc/test_cce_install-rosetta.R
@@ -2,22 +2,37 @@ library(reticulate)
 library(rosettaPTF)
 
 ARCPY_PATH <- "C:/Program Files/ArcGIS/Pro/bin/Python/envs/arcgispro-py3"
-PYENV_PATH <- "C:/Program Files/ArcGIS/Pro/bin/Python/envs/py_env"
 PYEXE_PATH <- file.path(ARCPY_PATH, "python.exe")
 CONDA_PATH <- "C:/Program Files/ArcGIS/Pro/bin/Python/Scripts/conda.exe"
 
 use_python(PYEXE_PATH, required = TRUE)
-
+use_condaenv(ARCPY_PATH)
 options(reticulate.conda_binary = CONDA_PATH)
 
-install_rosetta(pip = TRUE)
+# install rosetta if needed
+if (!rosettaPTF::rosetta_module_available())
+  install_rosetta(pip = TRUE)
 
 rose <- get_rosetta_module()
 
 run_rosetta(list(c(30, 30, 40, 1.5), c(55, 25, 20, 1.1)))
-predict(Rosetta(), list(c(30, 30, 40, 1.5), c(55, 25, 20, 1.1)))
+#>   id model_code theta_r_mean theta_s_mean log10_alpha_mean log10_npar_mean
+#> 1  1          3   0.11535773     0.417912        -2.067139       0.1120102
+#> 2  2          3   0.09130753     0.485032        -2.022388       0.1510716
+#>   log10_Ksat_mean theta_r_sd  theta_s_sd log10_alpha_sd log10_npar_sd
+#> 1       0.8325407 0.01335011 0.009377977     0.08251142    0.01323413
+#> 2       1.9060148 0.01277141 0.013062171     0.10020312    0.01763982
+#>   log10_Ksat_sd
+#> 1    0.09245277
+#> 2    0.14163567
 
-#> Fetching package metadata ...............
-#> Solving package specifications:
+predict(Rosetta(), list(c(30, 30, 40, 1.5), c(55, 25, 20, 1.1)))
+#> [[1]]
+#>            [,1]     [,2]      [,3]      [,4]      [,5]
+#> [1,] 0.11535773 0.417912 -2.067139 0.1120102 0.8325407
+#> [2,] 0.09130753 0.485032 -2.022388 0.1510716 1.9060148
 #>
-#>   InvalidSpecError: Invalid spec: =2.7
+#> [[2]]
+#>            [,1]        [,2]       [,3]       [,4]       [,5]
+#> [1,] 0.01335011 0.009377977 0.08251142 0.01323413 0.09245277
+#> [2,] 0.01277141 0.013062171 0.10020312 0.01763982 0.14163567
--- tests/testthat/test-install_rosetta.R
+++ tests/testthat/test-install_rosetta.R
@@ -1,5 +1,23 @@
 test_that("rosetta module is available", {
+
+  # check for ArcPro environment python EXE, and use it if present (USDA CCE machines)
+  ARCPY_PATH <- "C:/Program Files/ArcGIS/Pro/bin/Python/envs/arcgispro-py3"
+  PYEXE_PATH <- file.path(ARCPY_PATH, "python.exe")
+  CONDA_PATH <- "C:/Program Files/ArcGIS/Pro/bin/Python/Scripts/conda.exe"
+
+  if (file.exists(PYEXE_PATH)) {
+    message("\n\nUsing ArcGIS Pro conda environment/python.exe for testing...\n\n")
+    reticulate::use_python(PYEXE_PATH, required = TRUE)
+    reticulate::use_condaenv(ARCPY_PATH)
+    options(reticulate.conda_binary = CONDA_PATH)
+  }
+
+  # install rosetta if needed
+  if (!rosetta_module_available()) {
+    install_rosetta(pip = TRUE)
+  }
+
+  # should be available
   avail <- rosetta_module_available()
-  skip_if_not(avail)
   expect_true(avail)
 })
