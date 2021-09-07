fd739051efb8a22cbeb0293825d3708c8090b996
Andrew G. Brown <andrew.g.brown@usda.gov>
2021-09-06 22:11:46 -0700

    Update tests (skip on CRAN/without modules)

--- tests/testthat/test-install_rosetta.R
+++ tests/testthat/test-install_rosetta.R
@@ -1,4 +1,5 @@
 test_that("rosetta-soil module can be installed", {
+  skip_if_not(py_module_available("numpy"))
 
   # rosetta may be available
   avail <- rosetta_module_available()
@@ -7,8 +8,10 @@ test_that("rosetta-soil module can be installed", {
   cat("\n\n")
 
   # use pip (if available) or use ArcGIS Pro Conda environment (if available)
-  res <- install_rosetta(pip = TRUE, arcpy_path = "C:/Program Files/ArcGIS/Pro/bin/Python/")
+  res <- try(install_rosetta(pip = TRUE, arcpy_path = "C:/Program Files/ArcGIS/Pro/bin/Python/"))
 
+  if (inherits(res, 'try-error'))
+    skip("Unable to install")
   cat("\n\n")
 
   # res is TRUE if both rosetta-soil and numpy have been imported / can be loaded
@@ -22,6 +25,10 @@ test_that("rosetta-soil module can be installed", {
 })
 
 test_that("rosetta-soil module is available", {
+
+  skip_if_not(py_module_available("numpy"))
+  skip_if_not(py_module_available("rosetta"))
+
   # should be available
   avail <- rosetta_module_available()
   expect_true(avail)
--- tests/testthat/test-predict-Rosetta.R
+++ tests/testthat/test-predict-Rosetta.R
@@ -1,4 +1,8 @@
 test_that("prediction with Rosetta class works", {
+
+  skip_if_not(py_module_available("numpy"))
+  skip_if_not(py_module_available("rosetta"))
+
   one <- predict(Rosetta(), list(c(30, 30, 40, 1.5), c(55, 25, 20, 1.1)))
   two <- ann_predict(Rosetta(), list(c(30, 30, 40, 1.5), c(55, 25, 20, 1.1)))
 
