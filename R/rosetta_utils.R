7fe72b68c906bc98ed250f95b2a21b13dc1b274a
Andrew Gene Brown <brown.andrewg@gmail.com>
2023-08-15 15:45:08 -0700

    Add custom `py_to_r()` S3 wrapper for `rosetta.rosetta.SoilData` objects  - closes #8

--- R/rosetta_utils.R
+++ R/rosetta_utils.R
@@ -2,6 +2,8 @@
 
 #' Convert list of numeric vectors to _SoilData_ Python object
 #'
+#' @description `SoilDataFromArray`: convert a list of numeric vectors containing soil properties to a `rosetta.rosetta.SoilData` class
+#'
 #' @param x a list of numeric vectors
 #' @return an object reference to a Rosetta _SoilData_ Python object constructed from `x`
 #' @export
@@ -9,7 +11,15 @@ SoilDataFromArray <- function(x) {
   rosetta_module$SoilData$from_array(x)
 }
 
-#
+#' @description `py_to_r(<rosetta.rosetta.SoilData>)`: Wrapper S3 method for SoilData objects to prevent automatic conversion of SoilData (subclass of `"python.builtin.list"`) to an R `"list"`
+#' @importFrom reticulate py_to_r
+#' @export
+#' @rdname SoilDataFromArray
+py_to_r.rosetta.rosetta.SoilData <- function(x) {
+  # do nothing; fix for https://github.com/ncss-tech/rosettaPTF/issues/8
+  x
+}
+
 #' Check if Rosetta module is available for import from local Python environment
 #' @return _logical_
 #' @export
