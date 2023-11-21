2ab150d38e8be924ca41d9da01fa29b9878bbff2
Andrew G. Brown <andrew.g.brown@usda.gov>
2023-11-20 21:09:45 -0800

    Add `install_rosetta(system=)` argument for #10

--- R/install_rosetta.R
+++ R/install_rosetta.R
@@ -6,6 +6,7 @@
 #' @param method `"auto"`, `"virtualenv"`, or `"conda"`; Default: `"auto"`
 #' @param conda Default: `"auto"`
 #' @param pip _logical_. Use `pip` for package installation? Default: `TRUE`. This is only relevant when Conda environments are used, as otherwise packages will be installed from the Conda repositories.
+#' @param system _logical_. Default: `FALSE`. If `TRUE`, try installing to system (user) site library with `system()` and set reticulate to use system Python.
 #' @param arcpy_path Argument passed to `find_python()`. Path to ArcGIS Pro Python installation e.g. ``. Set as `NULL` (default) to prevent use of ArcGIS Pro instance.
 #' @details From `reticulate::py_install()`: On Linux and OS X the "virtualenv" method will be used by default ("conda" will be used if virtualenv isn't available). On Windows, the "conda" method is always used.
 #'
@@ -17,6 +18,7 @@ install_rosetta <- function(envname = NULL,
                             method = "auto",
                             conda = "auto",
                             pip = TRUE,
+                            system = FALSE,
                             arcpy_path = getOption("rosettaPTF.arcpy_path")) {
 
   # use heuristics to find python executable
@@ -24,11 +26,17 @@ install_rosetta <- function(envname = NULL,
     find_python(envname = envname, arcpy_path = arcpy_path)
   }
 
-  # get rosetta-soil (and numpy if needed)
-  try(reticulate::py_install("rosetta-soil", envname = envname, method = method, conda = conda, pip = pip),
-      silent = TRUE)
+  if (isFALSE(system)) {
+    # get rosetta-soil (and numpy if needed)
+    try(reticulate::py_install("rosetta-soil", envname = envname, method = method, conda = conda, pip = pip),
+        silent = TRUE)
+  } else {
+    p <- Sys.which("python")
+    reticulate::use_python(p, required = TRUE)
+    system(paste(shQuote(p), "-m pip install --upgrade --user rosetta-soil"))
+  }
 
-    # load modules globally in package (prevents having to reload rosettaPTF library in session)
+  # load modules globally in package (prevents having to reload rosettaPTF library in session)
   .loadModules()
 
 }
